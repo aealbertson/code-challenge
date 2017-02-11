16cc0fa6daaf82504e13246f7bba3a34b7054f2e
Ashton Albertson <ashtonalbertson@gmail.com>
2017-02-11 16:17:49 -0500

    adding files

---
+++ tempconverter.js
@@ -0,0 +1,9 @@
+var fahrenheitTemp = 100;
+
+console.log("Let's convert fahrenheit to celcius!");
+console.log("It's " + fahrenheitTemp + " degrees fahrenheit. What is the temperature in celcius?");
+
+var celciusTemp = ((fahrenheitTemp - 32) * 5)/9;
+
+console.log(fahrenheitTemp + " degrees fahrenheit is"+" "+ celciusTemp + " degrees celcius!");
+  
